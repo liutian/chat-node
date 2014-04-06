c12557414b93dc699be4ef5851a35f6ae5dec59f
liuss <liuss@visionet.com.cn>
2014-04-07 00:16:10 +0800

    用redis作为session管理仓库并对url方式传递的sid进行支持,

--- app.js
+++ app.js
@@ -9,7 +9,20 @@ var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 
+var RedisStore = require('connect-redis')(express);
+
 var app = express();
+
+//fetch sid from req.url for session
+var sidRegExp = /;sid=(.+)$/i;
+app.stack.unshift({ route: '', handle: function(req,res,next){
+	var m = req.url.match(sidRegExp);
+	if(m && m[1]){
+		req.headers.cookie = 'sid=' + m[1] + ';' + (req.headers.cookie ? req.headers.cookie : '');
+		req.url = req.url.replace(sidRegExp,'');
+	}
+	next();
+} });
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -21,7 +34,18 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(express.cookieParser('your secret here'));
-app.use(express.session());
+app.use(express.session({ key : 'sid',store: new RedisStore({host : '127.0.0.1',port : 6379,ttl : 1800}) }));
+
+app.use('/api',function(req,res,next){
+	if(!req.session.user){
+		console.log('api connect unauthorized');
+		res.json(result(401,'Unauthorized'));
+	}else{
+		console.log('api connect');
+		next();
+	}
+});
+
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -31,8 +55,27 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
-app.get('/users', user.list);
+app.post('/login', user.login);
+
+app.get('/api/user',user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+
+function result(code, msg) {
+	var result = {};
+
+	if(!code && !msg){
+		result.code = 10000;
+	}else if(!msg){
+		result.code = 10001;
+		result.msg = status;
+	}else{
+		result.code = code;
+		result.msg = msg;
+	}
+
+	return result;
+}
