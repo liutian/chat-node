26b754afa589f174683f93c2f27796092273aab4
liuss <liuss@visionet.com.cn>
2014-04-14 17:10:55 +0800

    前端添加私聊功能

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,15 +1,20 @@
 var module = angular.module('vsModule', []);
 module.controller('ctrl', function ($scope, $http) {
+	var socket = null;
+
 	$scope.login = function () {
 		$http({
 			url: '/login',
 			method: 'post',
 			data: {loginName: $scope.username, pwd: $scope.password}
 		}).success(function (data) {
-				console.log(data);
-			}).error(function () {
-				console.log('login error');
-			});
+			if(data.code == 10000){
+				console.log('login success');
+				initSocket();
+			}
+		}).error(function () {
+			console.log('login error');
+		});
 	}
 
 	$scope.getData = function () {
@@ -38,6 +43,30 @@ module.controller('ctrl', function ($scope, $http) {
 		onUploadSuccess: function (file, data, response, attr) {
 			data = JSON.parse(data);
 			console.log(data);
+		}
+	}
+
+	function initSocket(){
+		socket = io.connect('http://localhost:3000');
+
+		socket.on('whisper',function(data){
+			alert('whisper:' + data.content);
+		});
+
+		socket.on('broadcast',function(data){
+			alert('broadcast data:' + data);
+		})
+	}
+
+	$scope.whisper = function(){
+		if(socket){
+			socket.emit('whisper',{toUserId : $scope.toUserId,content : '你好'});
+		}
+	}
+
+	$scope.broadcast = function(){
+		if(socket){
+			socket.emit('broadcast',{content : '你好'});
 		}
 	}
 });
